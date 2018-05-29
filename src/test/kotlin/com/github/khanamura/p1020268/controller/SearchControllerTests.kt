c37183a597e8afc23a2e6caa2e9d056bd6cac0ed
Keisuke HANAMURA <khanamura@ap.equnix.com>
2018-05-29 16:49:08 +0900

    Adjusted the layout

--- src/test/kotlin/com/github/khanamura/p1020268/controller/SearchControllerTests.kt
+++ src/test/kotlin/com/github/khanamura/p1020268/controller/SearchControllerTests.kt
@@ -22,12 +22,11 @@ class SearchControllerTests {
 
   @Test
   fun homeTest() {
-    //HTTPCode200が返されること
     this.mockMvc.perform(get("/"))
       .andExpect(status().isOk)
-      .andExpect(xpath("/html/body/div/form/h1").string("郵便番号で住所が検索できます。"))
-      .andExpect(xpath("/html/body/div/form/input/@name").string("zip"))
-      .andExpect(xpath("/html/body/div/form/button").string("検索"))
+      .andExpect(xpath("/html/body/section/div/form/h1").string("郵便番号で住所が検索できます。"))
+      .andExpect(xpath("/html/body/section/div/form/div/div/input/@name").string("zip"))
+      .andExpect(xpath("/html/body/section/div/form/div/div/button").string("検索"))
       .andExpect(model().hasNoErrors<Any>())
   }
 
@@ -35,9 +34,9 @@ class SearchControllerTests {
   fun searchTest() {
     this.mockMvc.perform(post("/").param("zip", "1400011"))
       .andExpect(status().isOk)
-      .andExpect(xpath("/html/body/div/p/span[1]").string("東京都"))
-      .andExpect(xpath("/html/body/div/p/span[2]").string("品川区"))
-      .andExpect(xpath("/html/body/div/p/span[3]").string("東大井"))
+      .andExpect(xpath("/html/body/section/div/p/span[1]").string("東京都"))
+      .andExpect(xpath("/html/body/section/div/p/span[2]").string("品川区"))
+      .andExpect(xpath("/html/body/section/div/p/span[3]").string("東大井"))
       .andExpect(model().hasNoErrors<Any>())
   }
 }
