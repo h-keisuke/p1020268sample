65999251fc456192eb1db494db1639d1ff761158
Keisuke HANAMURA <khanamura@ap.equnix.com>
2018-06-04 10:10:16 +0900

    refactor code

--- src/main/kotlin/com/github/khanamura/p1020268/service/SearchService.kt
+++ src/main/kotlin/com/github/khanamura/p1020268/service/SearchService.kt
@@ -7,28 +7,22 @@ import com.github.khanamura.p1020268.domain.Address
 import com.github.khanamura.p1020268.domain.SearchResult
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
+import com.github.kittinunf.result.flatMap
 import org.springframework.stereotype.Service
 
 @Service
 class SearchService {
 
     fun search(zip: String): Result<List<Address>, Exception> {
-        val (request, response, result) =
+        val (_, _, result) =
             "http://zipcloud.ibsnet.co.jp/api/search?zipcode=$zip".httpGet().responseString()
-        return when (result) {
-            is Result.Failure -> {
-                Result.error(Exception(result.error))
-            }
-
-            is Result.Success -> {
-                val data = result.get()
-                val objectMapper = ObjectMapper().registerKotlinModule()
-                val searchResult = objectMapper.readValue<SearchResult>(data)
-                if (searchResult.status == 200) {
-                    Result.of(searchResult.results)
-                } else {
-                    Result.error(Exception(searchResult.message))
-                }
+        return result.flatMap {
+            val objectMapper = ObjectMapper().registerKotlinModule()
+            val searchResult = objectMapper.readValue<SearchResult>(it)
+            if (searchResult.status == 200) {
+                Result.of(searchResult.results)
+            } else {
+                Result.error(Exception(searchResult.message))
             }
         }
     }
